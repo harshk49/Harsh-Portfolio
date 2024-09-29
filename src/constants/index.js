5d9f72033c05ba182b9459a1380b7f5631f0c7d1
Harsh Kardile <harshkardile49@gmail.com>
2024-09-29 09:03:10 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/project-1.svg";
 import project2 from "../assets/projects/project-2.svg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
